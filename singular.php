c4ee0cefec2f380194bff3f27820ca7f8f797687
farhan-mohammed <farhanhm12@gmail.com>
2023-07-05 11:21:34 -0400

    remove singular page author and tags

--- singular.php
+++ singular.php
@@ -116,34 +116,6 @@
 
 						</div><!-- .post-content -->
 
-						<?php if (is_single()): ?>
-
-							<!-- <?php the_tags('<div class="post-tags">', '', '</div>'); ?> -->
-
-							<div class="post-author">
-
-								<a class="avatar" href="<?php echo $author_posts_url; ?>">
-									<?php echo get_avatar($author_id, 100); ?>
-								</a>
-
-								<h4 class="title"><a href="<?php echo $author_posts_url; ?>"><?php the_author_meta('display_name'); ?></a></h4>
-
-								<!-- <?php
-
-								$author_description = get_the_author_meta('description');
-
-								if ($author_description): ?>
-
-									<div class="post-author-description">
-										<?php echo wpautop($author_description); ?>
-									</div>
-
-								<?php endif; ?> -->
-
-							</div><!-- .post-author -->
-
-
-						<?php endif; ?>
 
 					</div><!-- .post-inner -->
 
